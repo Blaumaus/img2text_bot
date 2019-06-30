5834ed608e2f4909331362f2d005ea401541990d
Ookldev <35810911+Ookldev@users.noreply.github.com>
2019-06-30 19:09:33 +0300

    Config edit

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -2,7 +2,5 @@
 require('dotenv').config({ path: '.env' })
 
 module.exports = {
-  TOKEN: process.env.TOKEN || '',
-  PORT: process.env.PORT || 3000,
-  URL: process.env.URL || 'https://infinite-fortress-79609.herokuapp.com'
+  TOKEN: process.env.TOKEN || ''
 }
